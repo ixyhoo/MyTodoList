59a025b8d2dac3872e0a536e713e7bf1515445ad
ixyhoo <ixyhoo@outlook.fr>
2023-06-01 09:46:35 +0200

    debug is right

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+console.log('script start'); // Pour vérifier si le script est bien chargé
 const form = document.querySelector('form');
 const item = document.querySelector('#item');
 const list = document.querySelector('#list');
 
+//console.log(form, item, list); // Pour vérifier si les éléments sont bien récupérés
+
 form.addEventListener('submit', (e) => {
+   //console.log('form submit'); // Pour vérifier si l'événement submit est bien déclenché
    e.preventDefault();
 
    list.innerHTML += `<li>${item.value}</li>`;
-   item.value = '';
+   //console.log('item added', item.value); // Pour vérifier si un nouvel item est bien ajouté
 });
