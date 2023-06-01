e6046fedfcc5a8e18302ec8a49e48207af9dd0b8
ixyhoo <ixyhoo@outlook.fr>
2023-06-01 09:17:27 +0200

    debug js project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const item = document.querySelector('#item');
+const list = document.querySelector('#list');
 
 form.addEventListener('submit', (e) => {
    e.preventDefault();
