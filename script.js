98603e1c98abfb201cea219b8ec73541f8617139
ixyhoo <ixyhoo@outlook.fr>
2023-06-01 09:16:24 +0200

    debug js project

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const form = document.querySelector('form');
+const item = document.querySelector('#item');
 
 form.addEventListener('submit', (e) => {
    e.preventDefault();
-console.log(item.value);
-   // list.innerHTML = `<li>${item.value}</li>`;
+
+   list.innerHTML = `<li>${item.value}</li>`;
 });
